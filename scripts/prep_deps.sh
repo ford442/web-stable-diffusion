506d1f328f2b73ddd7843b8e7d882f79a179803a
Noah Cohn <noahc42@gmail.com>
2025-06-04 15:34:19 -0700

    Update prep_deps.sh

--- scripts/prep_deps.sh
+++ scripts/prep_deps.sh
@@ -11,9 +11,9 @@ TVM_HOME_SET="${TVM_HOME:-}"
 if [[ -z ${TVM_HOME_SET} ]]; then
     if [[ ! -d "3rdparty/tvm" ]]; then
         echo "Do not find TVM_HOME env variable, cloning a version as source".
-        git clone https://github.com/ford442/tvm 3rdparty/tvm --branch unity --recursive
+        git clone https://github.com/ford442/tvm /content/RAMDRIVE2/web-stable-diffusion/3rdparty/tvm --branch unity --recursive
     fi
-    export TVM_HOME="${TVM_HOME:-3rdparty/tvm}"
+    export TVM_HOME="/content/RAMDRIVE2/web-stable-diffusion/3rdparty/tvm"
 fi
 
 export TOKENZIER_WASM_HOME="/content/RAMDRIVE2/web-stable-diffusion/3rdparty/tokenizers-wasm"
